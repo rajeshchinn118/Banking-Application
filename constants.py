df22a60b9c2617adc6b826b4218d0a30732a2e06
Rajesh Bangi <128808906+rajeshchinn118@users.noreply.github.com>
2026-04-26 13:36:41 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,18 @@
+
+MAX_LOGIN_ATTEMPTS = 5
+
+#database configuration
+HOST = "localhost"
+USER = "root"
+PASSWORD = "root"
+DATABASE = "flm_bms"
+
+# MAIL CREDENTIALS
+SENDERMAIL = ''
+MAILPASS = ''
+
+# BANNER PATH
+BANNERPATH = 'E:/Python Projects/FLM Python projects/FLM Bank/FLM_Banking/com/Banner.txt'
+EXITBANNERPATH = 'E:/Python Projects/FLM Python projects/FLM Bank/FLM_Banking/com/exit_banner'
+
+
